1261376e8674152d0251eab97a89acbadd3c6449
Alex225511 <razboiule22@gmail.com>
2022-04-04 16:36:29 +0300

    adaugat printare 33

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm 22')
     print_hi('PyCharm 11')
+    print_hi('PyCharm 33')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
